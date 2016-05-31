b249b92a9e417fa12eb5469d4a0e23be9860011b
Dmytro Botnarenko <dmytro.botnarenko@gmail.com>
2016-05-31 15:35:51 +0300

    Gulp Watch and othe tasks
    Gulp Watch and othe tasks enabled. But without 'html' livereload
    functionality. Yet to do.

--- gulpfile.js
+++ gulpfile.js
@@ -2,11 +2,29 @@ var gulp = require('gulp');
 var autoprefixer = require('gulp-autoprefixer');
 var cleanCSS = require('gulp-clean-css');
 var rename = require('gulp-rename');
+var connect = require('gulp-connect');
 
 gulp.task('css', function () {
-    gulp.src('source/css/dimtest1.css')
-        .pipe(autoprefixer())
-        .pipe(cleanCSS())
-        .pipe(rename('style.css'))
-        .pipe(gulp.dest('build'))
+  gulp.src('source/css/*.css')
+    .pipe(autoprefixer())
+    .pipe(cleanCSS())
+    .pipe(rename('style.css'))
+    .pipe(gulp.dest('build'))
+    .pipe(connect.reload())
 });
+
+gulp.task('watch', function () {
+   gulp.watch('source/css/*.css', ['css']);
+});
+
+gulp.task('default', ['css']);
+
+gulp.task('connect', function() {
+  connect.server({
+    root: 'build',
+    livereload: true,
+    open: true
+  });
+});
+
+gulp.task('start', ['connect', 'watch']);
